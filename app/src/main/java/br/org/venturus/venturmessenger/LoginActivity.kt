07fce7738535f96095b4801596d0389ea57ae3c1
Joao Felipe <brjoaof@gmail.com>
2021-06-29 20:37:34 -0300

    Add Firebase Auth

---
+++ app/src/main/java/br/org/venturus/venturmessenger/LoginActivity.kt
@@ -0,0 +1,60 @@
+package br.org.venturus.venturmessenger
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        findViewById<Button>(R.id.btn_login).setOnClickListener {
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN
+            )
+        }
+
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+                val intent: Intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+                finish()
+            } else {
+                Toast.makeText(this, response?.error?.message, Toast.LENGTH_LONG ).show()
+            }
+        }
+    }
+
+}

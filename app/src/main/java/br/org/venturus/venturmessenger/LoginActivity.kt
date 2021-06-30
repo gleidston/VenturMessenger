e8cfe93a0d6ea973c7c7159df8e90fbc5f747c2b
Joao Felipe <brjoaof@gmail.com>
2021-06-30 19:57:44 -0300

    Fixed deprecated function startActivityForResult

--- app/src/main/java/br/org/venturus/venturmessenger/LoginActivity.kt
+++ app/src/main/java/br/org/venturus/venturmessenger/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
@@ -12,7 +13,10 @@ import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-    private val RC_SIGN_IN = 123
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            activityResult(result.resultCode, result.data)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,12 +28,11 @@ class LoginActivity : AppCompatActivity() {
         )
 
         findViewById<Button>(R.id.loginButton).setOnClickListener {
-            startActivityForResult(
+            resultLauncher.launch(
                 AuthUI.getInstance()
                     .createSignInIntentBuilder()
                     .setAvailableProviders(providers)
                     .build(),
-                RC_SIGN_IN
             )
         }
 
@@ -37,24 +40,17 @@ class LoginActivity : AppCompatActivity() {
     }
 
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun activityResult(resultCode: Int, data: Intent?) {
+        val response = IdpResponse.fromResultIntent(data)
 
-        if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
+        if (resultCode == Activity.RESULT_OK) {
+            //Success
+            val intent: Intent = Intent(this, MainActivity::class.java)
 
-            if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in
-                val user = FirebaseAuth.getInstance().currentUser
-                // ...
-                val intent: Intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-
-                finish()
-            } else {
-                Toast.makeText(this, response?.error?.message, Toast.LENGTH_LONG ).show()
-            }
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, response?.error?.message, Toast.LENGTH_LONG).show()
         }
     }
-
 }

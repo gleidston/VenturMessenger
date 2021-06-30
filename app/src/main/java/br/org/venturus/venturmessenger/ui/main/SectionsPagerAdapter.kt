d573ecbdd9e8b6c505d3aaf96f3e3e62aba7bc05
gleidston <king2010-ro@hotmail.com>
2021-06-30 18:09:45 -0300

    recycler adicionada

--- app/src/main/java/br/org/venturus/venturmessenger/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/br/org/venturus/venturmessenger/ui/main/SectionsPagerAdapter.kt
@@ -30,7 +30,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        // Show 3 total pages.
+        return 3
     }
 }

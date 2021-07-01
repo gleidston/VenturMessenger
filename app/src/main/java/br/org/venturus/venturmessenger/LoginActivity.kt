4b63d97809350ccad7dbf473936abd0b8a29d4f0
gleidston <king2010-ro@hotmail.com>
2021-07-01 18:29:23 -0300

    ajute do layout

--- app/src/main/java/br/org/venturus/venturmessenger/LoginActivity.kt
+++ app/src/main/java/br/org/venturus/venturmessenger/LoginActivity.kt
@@ -1,56 +1,79 @@
 package br.org.venturus.venturmessenger
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import br.org.venturus.venturmessenger.database.firestormDAO
-import br.org.venturus.venturmessenger.utils.Utils
-import br.org.venturus.venturmessenger.viewmodel.LoginViewModel
+import br.org.venturus.venturmessenger.model.User
+import br.org.venturus.venturmessenger.repository.UserRepository
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            activityResult(result.resultCode, result.data)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val resultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                activityResult(result.resultCode, result.data)
-            }
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
 
         findViewById<Button>(R.id.loginButton).setOnClickListener {
             resultLauncher.launch(
                 AuthUI.getInstance()
                     .createSignInIntentBuilder()
-                    .setAvailableProviders(Utils().getProviders())
+                    .setAvailableProviders(providers)
                     .build(),
             )
         }
+
+
     }
 
-    fun activityResult(resultCode: Int, data: Intent?) {
+
+    private fun activityResult(resultCode: Int, data: Intent?) {
         val response = IdpResponse.fromResultIntent(data)
 
         if (resultCode == Activity.RESULT_OK) {
-            firestormDAO().insertUser()
+            //Success
+            val current = FirebaseAuth.getInstance().currentUser?.apply {
+                val user: User = User(
+                    name = this.displayName ?: "NO NAME",
+                    email = this.email ?: "NO EMAIL",
+                    id = this.uid
+                )
+                UserRepository.addUser(user, {
+                    goToMain()
+                }, {
+                    failToLogin(it)
+                })
+            }
+
         } else {
             Toast.makeText(this, response?.error?.message, Toast.LENGTH_LONG).show()
         }
     }
 
-    fun goToMain() {
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+    private fun failToLogin(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
-    fun failToLogin(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    private fun goToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+
+        startActivity(intent)
+        finish()
     }
 }
